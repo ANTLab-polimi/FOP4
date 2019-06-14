4f755ee4f94a94f3decfa9b4648f94b183377c8f
Daniele Moro <daniele.moro92@gmail.com>
2019-06-13 19:56:32 -0700

    Update to install files

--- util/install.sh
+++ util/install.sh
@@ -148,7 +148,7 @@ function mn_deps {
     fi
 
     echo "Installing Mininet core"
-    pushd $MININET_DIR/P4-Containernet
+    pushd $MININET_DIR/FOP4
     sudo make install
     popd
 }
@@ -181,7 +181,7 @@ function of {
     cd $BUILD_DIR/openflow
 
     # Patch controller to handle more than 16 switches
-    patch -p1 < $MININET_DIR/P4-Containernet/util/openflow-patches/controller.patch
+    patch -p1 < $MININET_DIR/FOP4/util/openflow-patches/controller.patch
 
     # Resume the install:
     ./boot.sh
@@ -248,7 +248,7 @@ function install_wireshark {
     # Copy coloring rules: OF is white-on-blue:
     echo "Optionally installing wireshark color filters"
     mkdir -p $HOME/.wireshark
-    cp -n $MININET_DIR/P4-Containernet/util/colorfilters $HOME/.wireshark
+    cp -n $MININET_DIR/FOP4/util/colorfilters $HOME/.wireshark
 
     echo "Checking Wireshark version"
     WSVER=`wireshark -v | egrep -o '[0-9\.]+' | head -1`
@@ -496,9 +496,9 @@ function nox {
 
     # Apply patches
     git checkout -b tutorial-destiny
-    git am $MININET_DIR/P4-Containernet/util/nox-patches/*tutorial-port-nox-destiny*.patch
+    git am $MININET_DIR/FOP4/util/nox-patches/*tutorial-port-nox-destiny*.patch
     if [ "$DIST" = "Ubuntu" ] && version_ge $RELEASE 12.04; then
-        git am $MININET_DIR/P4-Containernet/util/nox-patches/*nox-ubuntu12-hacks.patch
+        git am $MININET_DIR/FOP4/util/nox-patches/*nox-ubuntu12-hacks.patch
     fi
 
     # Build
